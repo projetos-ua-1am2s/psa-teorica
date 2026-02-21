6f7666d0d247cf3367c5f3ffb286e149ac5425c6
diana-baldaia <dianabaldaia@ua.pt>
2026-02-21 18:56:44 +0000

    Fuga adicionada

--- move_player.m
+++ move_player.m
@@ -4,56 +4,100 @@ if s(i).killed == 1 % to stop the movement
     return
 end
 
-%% Estratégia apenas de caça (por enquanto)
+
+% Defines the target color and the color to avoid
+if s(i).color == 'r'
+    target = 'g';
+    avoid = 'b';
+elseif s(i).color == 'g'
+    target = 'b';
+    avoid = 'r';
+else
+    target = 'r';
+    avoid = 'g';
+end
+
+
+%% Estratégia simples de caça e fuga
 % Tudo isto pode passar para dentro do "if s(i).color == 'r'" para termos
 % outras estratégias para as outras cores
 
-kill_dst = 0.3;
 
+% ----- Saves the potencial preys and predators -----
 k = 1;
+r = 1;
 
-% Defines the target color
-if s(i).color == 'r'
-    target = 'g';
-elseif s(i).color == 'g'
-    target = 'b';
-else
-    target = 'r';
-end
-
-% Saves the potencial preys
 for j = 1:size(s,2)
+    % Saves the index and the distance of all the preys
     if s(j).color == target && s(j).killed == 0
         preys(k).idx = j;
         preys(k).dst = sqrt((s(i).x - s(j).x)^2 + (s(i).y - s(j).y)^2);
-        k = k + 1;
+        k = k + 1;   
+    % Saves the index and the distance of all the predators
+    elseif s(j).color == avoid && s(j).killed == 0
+        predators(r).idx = j;
+        predators(r).dst = sqrt((s(i).x - s(j).x)^2 + (s(i).y - s(j).y)^2);
+        r = r + 1;
     end
 end
 
-% If there are no preys alive
-if k == 1
-    return
+
+kill_dst = 1;             % kill range
+avoid_dst = 4 * kill_dst;   % avoidance range
+
+% ----- Closest prey (if any) -----
+if k > 1
+    dist_preys = [preys.dst];
+    [min_dst_t, idx_k] = min(dist_preys);
+    idx_t = preys(idx_k).idx;
+
+    % Kills the prey if it is close enough
+    if min_dst_t < kill_dst
+        s(idx_t).killed = 1;
+        return
+    end
+else
+    idx_t = [];
 end
 
-% Closest prey
-dists = [preys.dst];
-[min_dst, idx_k] = min(dists);
-idx = preys(idx_k).idx;
-
-% Kills the prey if it is close enough
-if min_dst < kill_dst
-    s(idx).killed = 1;
-    return
+% ----- Closest predator (if any) -----
+if r > 1
+    dist_pred = [predators.dst];
+    [min_dst_a, idx_r] = min(dist_pred);
+    idx_a = predators(idx_r).idx;
+else
+    min_dist_a = inf;
 end
 
-% Move towards the prey
-prey_x = s(idx).x;
-prey_y = s(idx).y;
-ang_to_prey = atan2( prey_y - s(i).y , prey_x - s(i).x);
+% ----- Defines the movement -----
+% Check if the closest predator is within a certain distance
+if min_dst_a < avoid_dst
+    % Move away from the predator
+    predator_x = s(idx_a).x;
+    predator_y = s(idx_a).y;
+    ang_to_predator = atan2(s(i).y - predator_y, s(i).x - predator_x);
+    
+    s(i).x = s(i).x + vmax * cos(ang_to_predator);
+    s(i).y = s(i).y + vmax * sin(ang_to_predator);
 
-s(i).x = s(i).x + vmax * cos(ang_to_prey);
-s(i).y = s(i).y + vmax * sin(ang_to_prey);
-s(i).ang = ang_to_prey;
+elseif ~isempty(idx_t)
+    % Move towards the prey
+    prey_x = s(idx_t).x;
+    prey_y = s(idx_t).y;
+    ang_to_prey = atan2( prey_y - s(i).y , prey_x - s(i).x);
+    
+    s(i).x = s(i).x + vmax * cos(ang_to_prey);
+    s(i).y = s(i).y + vmax * sin(ang_to_prey);
+    s(i).ang = ang_to_prey;
+    
+else
+    % Move in a random direction if no prey or predator is nearby
+    s(i).ang = rand * 2 * pi;
+    s(i).x = s(i).x + vmax * cos(s(i).ang);
+    s(i).y = s(i).y + vmax * sin(s(i).ang);
+end
+
+
 
 
 
